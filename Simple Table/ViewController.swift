df7f972262ee6d4d085a5e1e571cba2d4816109b
avi <avinashsajjan@gmail.com>
2015-02-11 21:49:55 +0530

    added table data and now it displays simple table.

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl",
+        "Petite Oyster", "For Kee Restaurant", "Po's Atelier",
+        "Bourke Street Bakery", "Haigh’s Chocolate", "Palomino Espresso",
+        "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf",
+        "Five Leaves", "Cafe Lore", "Confessional", "Barrafina",
+        "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return restaurantNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = restaurantNames[indexPath.row]
+        return cell
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
